d75d43a5e331f384287194615b18969ab23bb798
aripan <aripan017@gmail.com>
2021-08-20 15:01:36 +0200

    Data fetched depending on date range

--- src/components/FetchToRenderData.js
+++ src/components/FetchToRenderData.js
@@ -1,21 +1,52 @@
-import React, { useEffect, useState } from 'react'
+import DateRangePicker from "@wojtekmaj/react-daterange-picker";
+import React, { useEffect, useState } from "react";
 
 const FetchToRenderData = () => {
-    const [fetchedData, setFetchedData] = useState({})
+  const [fetchedData, setFetchedData] = useState({});
+  const [date, setDate] = useState([new Date(), new Date()]);
 
-    useEffect(() => {
-       const URL = "https://api.coindesk.com/v1/bpi/historical/close.json?start=2020-11-25&end=2020-12-04"
+  const formatDate = (dateInput) => {
+    const newDate = new Date(dateInput);
+    const monthValue =
+      newDate.getMonth() < 10
+        ? "0" + (newDate.getMonth() + 1).toString()
+        : (newDate.getMonth() + 1).toString;
 
-       fetch(URL).then(res=> res.json()).then(data => setFetchedData(data.bpi))
-       
-    }, [])
+    const dateValue = newDate.getDate() < 10
+    ? "0" + (newDate.getDate()).toString()
+    : (newDate.getDate());
+    return `${newDate.getFullYear()}-${monthValue}-${dateValue}`;
+  };
 
-    console.log(fetchedData)
-    return (
-        <div>
-            <button>Render</button>
-        </div>
-    )
+  useEffect(() => {
+    const startDate = formatDate(date[0]);
+    const endDate = formatDate(date[1]);
+
+    let URL;
+if(startDate === endDate){
+    URL =`https://api.coindesk.com/v1/bpi/historical/close.json`
+} else{
+    URL = `https://api.coindesk.com/v1/bpi/historical/close.json?start=${startDate}&end=${endDate}`;
 }
+    try {
+      fetch(URL)
+        .then((res) => res.json())
+        .then((data) => setFetchedData(data.bpi))
+      
+    } catch (error) {
+      console.log("Please select a date range");
+    }
 
-export default FetchToRenderData
+    console.log(URL)
+  }, [date]);
+
+  console.log(fetchedData)
+  return (
+    <div>
+      <DateRangePicker onChange={setDate} value={date} />
+      <button>Render</button>
+    </div>
+  );
+};
+
+export default FetchToRenderData;
